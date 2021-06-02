7fdbd2302d8b6e733b9b04a3fe632c043b70fc73
cjhoon96 <cjhoon96@gmail.com>
2021-06-02 22:14:48 +0900

    i need to add sound

---
+++ pop_cat.py
@@ -0,0 +1,42 @@
+import pygame as pg
+import sys
+
+pg.init()
+
+screen = pg.display.set_mode((680,665))
+
+pop_open = pg.image.load("./pop_img/popcat_open.png")
+pop_close = pg.image.load("./pop_img/popcat_close.png")
+
+scr = pop_open
+
+done = False
+click = False
+while not done:
+    screen.fill((0,0,0))
+    
+    screen.blit(scr, (0,0))
+    pg.display.flip()
+    
+    for event in pg.event.get():
+        if event.type == pg.MOUSEBUTTONDOWN:# If user release what he pressed.
+            click = True
+        elif event.type == pg.MOUSEBUTTONUP:# If user press any key.
+            click= False
+        elif event.type == pg.QUIT: # If user clicked close.
+            done= True 
+
+    if click:
+        scr = pop_open
+        screen.fill((0,0,0))
+        screen.blit(scr, (0,0))
+        pg.display.flip()
+    elif not click:
+        scr = pop_close
+        screen.fill((0,0,0))
+        screen.blit(scr, (0,0))
+        pg.display.flip()
+
+
+pg.quit()
+sys.exit()
